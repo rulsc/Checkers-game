c1dc7ccf9b8340b98565eefc26b92e2573f7fa5f
rulsc <rcort44@wgu.edu>
2023-03-08 16:51:58 -0800

    new array activeCheckerSquares and setTimeout

--- classes/utility/BasicAI.js
+++ classes/utility/BasicAI.js
@@ -15,30 +15,35 @@ BasicAI.prototype.lowerCover = function () {
 BasicAI.prototype.move = function () {
   const self = this;
   const player = self.player;
-  let randomNum;
-  let activeSquares;
-  let activeEmptySquares;
+  let randomNum, activeSquares, activeCheckerSquares, activeEmptySquares;
   self.raiseCover();
   const intervalId = setInterval(function () {
     if (player.isActive) {
       activeSquares = [...document.getElementsByClassName("highlight")];
+      activeCheckerSquares = activeSquares.filter(
+        (square) => square.firstChild
+      );
       activeEmptySquares = [];
-      randomNum = Math.floor(Math.random() * activeSquares.length);
-      activeSquares[randomNum].click();
-      activeSquares = [];
-      activeSquares = [...document.getElementsByClassName("highlight")];
-      activeSquares.forEach((el) => {
-        if (!el.firstChild) {
-          activeEmptySquares.push(el);
-        }
-      });
-      randomNum = Math.floor(Math.random() * activeEmptySquares.length);
-      activeEmptySquares[randomNum].click();
-      activeSquares = [];
-      activeEmptySquares = [];
+      randomNum = Math.floor(Math.random() * activeCheckerSquares.length);
+      activeCheckerSquares[randomNum].click();
+
+      setTimeout(function () {
+        activeSquares = [];
+        activeCheckerSquares = [];
+        activeSquares = [...document.getElementsByClassName("highlight")];
+        activeSquares.forEach((el) => {
+          if (!el.firstChild) {
+            activeEmptySquares.push(el);
+          }
+        });
+        randomNum = Math.floor(Math.random() * activeEmptySquares.length);
+        activeEmptySquares[randomNum].click();
+        activeSquares = [];
+        activeEmptySquares = [];
+      }, 500);
     } else {
       clearInterval(intervalId);
       self.lowerCover();
     }
-  }, 500);
+  }, 1000);
 };
